44dc30dc955131636ba9f4620ce0be7d8bbd9c5b
Sophien <bouderbala.s@gmail.com>
2025-06-24 15:26:30 +0200

    ajout fichier partials

---
+++ _depencies.php
@@ -0,0 +1,3 @@
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<script src="https://kit.fontawesome.com/a6c393237d.js"></script>
+<script defer src= "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
